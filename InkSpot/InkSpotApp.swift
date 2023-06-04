725391209c5f7df74044b5ddf1ebccce1b2983e8
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-06-04 16:43:50 -0400

    Initial Commit

---
+++ InkSpot/InkSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  InkSpotApp.swift
+//  InkSpot
+//
+//  Created by Lori Rothermel on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct InkSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
